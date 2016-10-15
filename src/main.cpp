0d86a2f44c36f12b5c78a4d965c8db271260674f
Juan <juanmvj_1996@live.com.mx>
2016-10-15 01:23:54 -0500

    se agrego arbolB

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+	Nombres(s): Altamirnao Leal Jose ALfonso
+	Nombres(s): Vázquez Jiménez Juan Manuel
+	
+*/
+#include "Arbol.h"
+
+int main()
+{
+	Arbol ab = Arbol(10);
+
+	ab.ShowA();
+
+	ab.imprime();
+	return 0;
+}
